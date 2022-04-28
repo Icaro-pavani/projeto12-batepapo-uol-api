6bac27501250af6a4b09d72181f52192e991fbc2
Icaro-pavani <icaro.pavani79@gmail.com>
2022-04-28 02:09:54 -0300

    feat: post new participants

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 import express, { json } from "express";
+import { MongoClient } from "mongodb";
 import cors from "cors";
 import chalk from "chalk";
 import Joi from "joi";
+import dayjs from "dayjs";
+import dotenv from "dotenv";
+
+dotenv.config();
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 const schema = Joi.object({
   name: Joi.string().required(),
 });
 
-const users = [];
-
 const app = express();
 app.use(json());
 app.use(cors());
 
-app.post("/participants", (req, res) => {
-  const { error, value } = schema.validate(req.body);
-  console.log(value);
-  if (error) {
-    res.status(422).send(error.message);
-    return;
+app.post("/participants", async (req, res) => {
+  try {
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      res.status(422).send(error.message);
+      return;
+    }
+    const { name } = value;
+    await mongoClient.connect();
+    const dbChat = mongoClient.db("projeto12-batepapo-uol-api");
+    const dbChatUsers = dbChat.collection("Users");
+    const dbChatMessages = dbChat.collection("Messages");
+    const user = await dbChatUsers.findOne({ name });
+    if (!user) {
+      const newUser = {
+        name,
+        lastStatus: Date.now(),
+      };
+      const message = {
+        from: value.name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs(Date.now()).format("hh:mm:ss"),
+      };
+      await dbChatUsers.insertOne(newUser);
+      await dbChatMessages.insertOne(message);
+      res.sendStatus(201);
+    } else {
+      res.sendStatus(409);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
   }
-  res.sendStatus(200);
-  users.push({ name: value.name, lastStatus: Date.now(0) });
-  console.log(users);
 });
 
 app.listen(5000, () => console.log(chalk.bold.green("Servidor Online!")));
